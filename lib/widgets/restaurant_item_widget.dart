e77c8dcf204a88e23f21deaf11bd56ddf89828ca
AdlyRadwika <85500151+AdlyRadwika@users.noreply.github.com>
2022-09-25 10:43:01 +0700

    refactor minor thing #2

--- lib/widgets/restaurant_item_widget.dart
+++ lib/widgets/restaurant_item_widget.dart
@@ -24,106 +24,103 @@ class RestaurantItemWidget extends StatelessWidget {
           future: provider.isFavorite(resto.id),
           builder: (context, snapshot) {
             var isFavorite = snapshot.data ?? false;
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                children: [
-                  ListTile(
-                    onTap: () {
-                      DetailPageArguments arguments =
-                          DetailPageArguments(restoId: resto.id, index: index);
-                      Navigation.intentWithData(route.detailPage, arguments);
-                    },
-                    isThreeLine: true,
-                    leading: Hero(
-                      tag: resto.pictureId,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          ApiService()
-                              .restaurantImage('small', resto.pictureId),
-                          width: 150,
-                          fit: BoxFit.cover,
-                        ),
+            return Column(
+              children: [
+                ListTile(
+                  onTap: () {
+                    DetailPageArguments arguments =
+                        DetailPageArguments(restoId: resto.id, index: index);
+                    Navigation.intentWithData(route.detailPage, arguments);
+                  },
+                  isThreeLine: true,
+                  leading: Hero(
+                    tag: resto.pictureId,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        ApiService()
+                            .restaurantImage('small', resto.pictureId),
+                        width: 125,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    title: Text(
-                      resto.name,
-                      style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                            fontWeight: FontWeight.w600,
+                  ),
+                  title: Text(
+                    resto.name,
+                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          fontWeight: FontWeight.w600,
+                        ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  subtitle: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            size: 16,
                           ),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    ),
-                    subtitle: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            const Icon(
-                              Icons.location_on,
-                              size: 16,
-                            ),
-                            const SizedBox(
-                              width: 1,
-                            ),
-                            Text(
-                              resto.city,
-                              style: Theme.of(context).textTheme.overline,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            )
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            const Icon(
-                              Icons.star,
-                              size: 16,
-                            ),
-                            const SizedBox(
-                              width: 1,
-                            ),
-                            Text(
-                              resto.rating.toString(),
-                              style: Theme.of(context).textTheme.overline,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                    trailing: isFavorite
-                        ? IconButton(
-                            onPressed: () {
-                              provider.removeFavorite(resto.id);
-                            },
-                            icon: const Icon(
-                              Icons.favorite,
-                              size: 26,
-                              color: Colors.redAccent,
-                            ),
-                            splashColor: Colors.transparent,
-                            tooltip: 'Favorite',
+                          const SizedBox(
+                            width: 1,
+                          ),
+                          Text(
+                            resto.city,
+                            style: Theme.of(context).textTheme.overline,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
                           )
-                        : IconButton(
-                            onPressed: () {
-                              provider.addFavorite(resto);
-                            },
-                            icon: const Icon(
-                              Icons.favorite_border,
-                              size: 26,
-                              color: Colors.redAccent,
-                            ),
-                            splashColor: Colors.transparent,
-                            tooltip: 'Favorite',
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            size: 16,
                           ),
+                          const SizedBox(
+                            width: 1,
+                          ),
+                          Text(
+                            resto.rating.toString(),
+                            style: Theme.of(context).textTheme.overline,
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                  const Divider(
-                    height: 2,
-                  ),
-                ],
-              ),
+                  trailing: isFavorite
+                      ? IconButton(
+                          onPressed: () {
+                            provider.removeFavorite(resto.id);
+                          },
+                          icon: const Icon(
+                            Icons.favorite,
+                            size: 26,
+                            color: Colors.redAccent,
+                          ),
+                          splashColor: Colors.transparent,
+                          tooltip: 'Favorite',
+                        )
+                      : IconButton(
+                          onPressed: () {
+                            provider.addFavorite(resto);
+                          },
+                          icon: const Icon(
+                            Icons.favorite_border,
+                            size: 26,
+                            color: Colors.redAccent,
+                          ),
+                          splashColor: Colors.transparent,
+                          tooltip: 'Favorite',
+                        ),
+                ),
+                const Divider(
+                  height: 2,
+                ),
+              ],
             );
           },
         );
